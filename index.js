1ed8a1897944bfb2380691b8b669b47adda31e91
Ryan Achten <ryanachten@gmail.com>
2018-12-31 13:03:57 +1300

    Add dynamic port for Heroku, Add start script to package

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const app = require('./server/server');
+const app = require("./server/server");
 
-app.listen(4000, () => {
-  console.log('Listening');
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log("Listening");
 });
