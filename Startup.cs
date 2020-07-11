82151d36125d8ba8a545f3809e0a38231d72e855
Ryan Achten <ryanachten@gmail.com>
2020-07-11 16:51:55 +1200

    Register exercise services on startup

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RobicServer.Models;
+using RobicServer.Services;
 
 namespace RobicServer
 {
@@ -25,6 +21,14 @@ namespace RobicServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configures database using settings defined in the appsettings config
+            services.Configure<ExerciseDatabaseSettings>(Configuration.GetSection(nameof(ExerciseDatabaseSettings)));
+            // Database interface is registered with DI for a singleton service lifetime
+            services.AddSingleton<IExerciseDatabaseSettings>(
+                serviceProvider => serviceProvider.GetRequiredService<IOptions<ExerciseDatabaseSettings>>().Value
+            );
+            services.AddSingleton<ExerciseService>();
+
             services.AddControllers();
         }
 
