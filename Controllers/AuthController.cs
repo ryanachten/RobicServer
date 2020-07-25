64388ffb2e7a928ce68d025a596e396e95c5f5ad
Ryan Achten <ryanachten@gmail.com>
2020-07-26 10:47:07 +1200

    Add claims and token generation to login method

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,19 +4,26 @@ using Microsoft.AspNetCore.Mvc;
 using RobicServer.Services;
 using RobicServer.Models;
 using RobicServer.Models.DTOs;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace RobicServer.Controllers
 {
-
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IConfiguration _config;
         private readonly IMapper _mapper;
         private readonly IAuthRepository _repo;
 
-        public AuthController(IAuthRepository repo, IMapper mapper)
+        public AuthController(IAuthRepository repo, IMapper mapper, IConfiguration config)
         {
+            _config = config;
             _mapper = mapper;
             _repo = repo;
         }
@@ -37,9 +44,32 @@ namespace RobicServer.Controllers
             if (user == null)
                 return Unauthorized();
 
-            // TODO: add claims and token handling
+            var claims = new[]{
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
 
-            return Ok(user);
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config.GetSection("TokenKey").Value)
+            );
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddYears(1),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token),
+                user
+            });
         }
     }
 }
