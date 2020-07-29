43699cd974085f088a2e66c216b98a97af6a5efe
Ryan Achten <ryanachten@gmail.com>
2020-07-29 18:55:43 +1200

    Add delete user controller method

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,6 +6,8 @@ using AutoMapper;
 using RobicServer.Models.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
+using System;
+using System.Collections.Generic;
 
 namespace RobicServer.Controllers
 {
@@ -16,11 +18,20 @@ namespace RobicServer.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IMongoRepository<User> _userRepo;
+        private readonly IMongoRepository<Exercise> _exerciseRepo;
+        private readonly IMongoRepository<ExerciseDefiniton> _exerciseDefinitionRepo;
 
-        public UserController(IMongoRepository<User> userRepo, IMapper mapper)
+        public UserController(
+            IMongoRepository<User> userRepo,
+            IMongoRepository<Exercise> exerciseRepo,
+            IMongoRepository<ExerciseDefiniton> exerciseDefinitionRepo,
+            IMapper mapper
+        )
         {
             _mapper = mapper;
             _userRepo = userRepo;
+            _exerciseRepo = exerciseRepo;
+            _exerciseDefinitionRepo = exerciseDefinitionRepo;
         }
 
         [HttpGet("{id:length(24)}", Name = "GetUser")]
@@ -36,6 +47,34 @@ namespace RobicServer.Controllers
 
             var userForReturn = _mapper.Map<UserForDetailDto>(user);
             return Ok(userForReturn);
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (userId != id)
+                return Unauthorized();
+
+            User user = await _userRepo.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            // Clean up associated user data
+            foreach (var definitionId in user.Exercises)
+            {
+                var definiton = await _exerciseDefinitionRepo.FindByIdAsync(definitionId);
+                foreach (var exerciseId in definiton.History)
+                {
+                    // Remove all associated exercise sesssions
+                    await _exerciseRepo.DeleteByIdAsync(exerciseId);
+                }
+                // Remove all associated exercise definitions
+                await _exerciseDefinitionRepo.DeleteByIdAsync(definitionId);
+            }
+
+            await _userRepo.DeleteByIdAsync(user.Id);
+            return NoContent();
         }
     }
 }
