9ae247891a2265ca18292bdec55a03833ff2e724
Ryan Achten <ryanachten@gmail.com>
2021-02-20 09:21:26 +1300

    Add email to user detail DTO

--- Models/DTOs/UserForDetailDto.cs
+++ Models/DTOs/UserForDetailDto.cs
@@ -6,9 +6,8 @@ namespace RobicServer.Models.DTOs
     {
         public string Id { get; set; }
         public string FirstName { get; set; }
-
         public string LastName { get; set; }
-
+        public string Email { get; set; }
         public ICollection<string> Exercises { get; set; }
     }
 }
