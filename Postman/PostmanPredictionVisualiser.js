c825e096e06af1697ed45cc0fcefcf2c763ff6c6
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2021-06-13 15:21:10 +1200

    Add legend to postman visualiser

--- Postman/PostmanPredictionVisualiser.js
+++ Postman/PostmanPredictionVisualiser.js
@@ -16,43 +16,41 @@ const vizData = {
 };
 
 var template = `
-<canvas id="myChart" height="150"></canvas>
-
+<canvas id="chart"></canvas>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script> 
-
 <script>
-    var ctx = document.getElementById("myChart");
+    var ctx = document.getElementById("chart");
 
     var myChart = new Chart(ctx, {
         type: "line",
         data: {
             labels: [],
             datasets: [{
-                    label: '',
+                    label: 'Actual',
                     data: [],
 					borderColor: "blue",
 					fill: false,         
                 },
                 {
-                    label: '',
+                    label: 'Forecasted',
                     data: [],
                     borderColor: "purple",
                     fill: false,         
                 },
                 {
-                    label: '',
+                    label: 'Predicted',
                     data: [],
                     borderColor: "orange",
                     fill: false,         
                 },
                 {
-                    label: '',
+                    label: 'Predicted (Lower band)',
                     data: [],
                     borderColor: "yellow",
                     fill: false,         
                 },
                 {
-                    label: '',
+                    label: 'Predicted (Upper band)',
                     data: [],
                     borderColor: "red",
                     fill: false,         
@@ -60,7 +58,7 @@ var template = `
             ]
         },
         options: {
-            legend: { display: false },
+            legend: { display: true },
             title: {
                 display: true,
                 text: ''
@@ -87,16 +85,11 @@ var template = `
 
     // Access the data passed to pm.visualizer.set() from the JavaScript
     // code of the Visualizer template
-    pm.getData(function (err, value) {
-        myChart.data.datasets[0].label = value.labels
+    pm.getData((err, value) => {
         myChart.data.datasets[0].data = value.actualResults;
-        myChart.data.datasets[1].label = value.labels
         myChart.data.datasets[1].data = value.forecastedResults;
-        myChart.data.datasets[2].label = value.predictedLabels;
         myChart.data.datasets[2].data = value.predictedEstimate;
-        myChart.data.datasets[3].label = value.predictedLabels;
         myChart.data.datasets[3].data = value.lowerEstimate;
-        myChart.data.datasets[4].label = value.predictedLabels;
         myChart.data.datasets[4].data = value.upperEstimate;
         myChart.data.labels = value.labels;
         myChart.update();
